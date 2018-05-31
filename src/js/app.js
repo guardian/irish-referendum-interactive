d665a6d917965a1e9fda89fe75b370404464816f
seanclarkeguardian <sean.clarke@guardian.co.uk>
2018-05-31 10:12:52 +0100

    final commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,34 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-// WEDS AM: use the remote url to test!
+// // WEDS AM: use the remote url to test!
 
-var resultsurl = 'https://interactive.guim.co.uk/atoms/2018/05/irish-abortion-referendum/static/results.json';
-var yesbar = document.querySelector('.gv-bar.gv-yes');
-var nobar = document.querySelector('.gv-bar.gv-no');
-var yesvalue = document.querySelector('.gv-answer-value.gv-yes');
-var novalue = document.querySelector('.gv-answer-value.gv-no');
-var timestamp = document.querySelector('.gv-timestamp');
-var progress = document.querySelector('.gv-declared');
-
-
-function fetchdata() {
-    axios.get(resultsurl)
-    .then(function(json){
-        yesbar.style.width = json.data.votes_in_favour_percent + "%";
-        nobar.style.width = json.data.votes_against_percent + "%";
-        yesvalue.innerText = json.data.votes_in_favour_percent + "%";
-        novalue.innerText = json.data.votes_against_percent + "%";
-        timestamp.innerText = 'at ' + json.data.timestamp;
-        progress.innerText = json.data.constituenciesDeclared;
-    })
-    .catch(function (err) {
-        console.log(err)
-    })
-}
+// var resultsurl = 'https://interactive.guim.co.uk/atoms/2018/05/irish-abortion-referendum/static/results.json';
+// var yesbar = document.querySelector('.gv-bar.gv-yes');
+// var nobar = document.querySelector('.gv-bar.gv-no');
+// var yesvalue = document.querySelector('.gv-answer-value.gv-yes');
+// var novalue = document.querySelector('.gv-answer-value.gv-no');
+// var timestamp = document.querySelector('.gv-timestamp');
+// var progress = document.querySelector('.gv-declared');
 
 
-setInterval(function() {
-    fetchdata();
-},150000)
+// function fetchdata() {
+//     axios.get(resultsurl)
+//     .then(function(json){
+//         yesbar.style.width = json.data.votes_in_favour_percent + "%";
+//         nobar.style.width = json.data.votes_against_percent + "%";
+//         yesvalue.innerText = json.data.votes_in_favour_percent + "%";
+//         novalue.innerText = json.data.votes_against_percent + "%";
+//         timestamp.innerText = 'at ' + json.data.timestamp;
+//         progress.innerText = json.data.constituenciesDeclared;
+//     })
+//     .catch(function (err) {
+//         console.log(err)
+//     })
+// }
 
-fetchdata();
+
+// setInterval(function() {
+//     fetchdata();
+// },150000)
+
+// fetchdata();
